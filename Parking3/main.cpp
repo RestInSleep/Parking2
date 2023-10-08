fb47ef4f57a71fb8aa8dea43340fa8d5bf1f0fdd
Jan <jasjago@gmail.com>
2023-10-08 14:22:00 +0200

    sprawdzanie regexa

--- Parking3/main.cpp
+++ Parking3/main.cpp
@@ -10,6 +10,15 @@
 #include <vector>
 
 
+bool isEnteringPaymentOk(std::string& s) {
+    
+    return std::regex_match(s, std::regex("\\s*([A-Z][A-Z0-9]{2,10})\\s*((0?[89]|1[0-9])\\.([0-5][0-9])|20\\.00)\\s*((0?[89]|1[0-9])\\.([0-5][0-9])|20\\.00)\\s*"));
+    
+}
+
+
 int main() {
+    std::string name{"TK56 20.00 10.56"};
+    std::cout << isEnteringPaymentOk(name);
     return 0;
 }
